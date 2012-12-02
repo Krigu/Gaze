4ae8f376ab1d2c2a55b57aa7cb87f8d18e898a57
Krigu <chris.feuz@gmail.com>
2012-12-02 16:31:02 +0100

    Added calibration javascript

---
+++ JS/js/calibration.js
@@ -0,0 +1,47 @@
+var calibrationCircle = {
+	
+	imgPath: "img/circle_full.jpg", //url to image here
+	imgMovingPath: "img/circle.jpg", //url to moving image here
+	imgWidth: 40, //standard width
+	imgHeight: 40, //standard height
+		
+	init : function(posX, posY){
+		//create the image cloud
+		var circle = document.createElement("IMG");
+		$(circle).attr("src", calibrationCircle.imgPath);
+		$(circle).attr("alt", "circle");
+		$(circle).attr("class", "circle");
+		$(circle).attr("id", "circleImg");
+
+		$(document.body).append($(circle));
+
+		calibrationCircle.display(posX, posY);		
+
+	},
+	
+	display : function(posX, posY) {
+		$("#circleImg").css("top", posY + (calibrationCircle.imgHeight / 2));
+		$("#circleImg").css("left", posX + (calibrationCircle.imgWidth / 2));
+		$("#circleImg").show(0);
+	},
+
+	move : function(posX, posY){
+		$("#circleImg").attr("src", calibrationCircle.imgMovingPath);
+		$("#circleImg").animate({ 
+			top: posY + (calibrationCircle.imgHeight / 2),
+			left: posX + (calibrationCircle.imgWidth / 2)
+		}, 2500, function() {
+			$("#circleImg").attr("src", calibrationCircle.imgPath);
+		 });
+	
+	},
+
+	hide : function(){
+		$("#circleImg").hide();
+	}
+}
+
+$(document).ready(function() {
+	   calibrationCircle.init();
+});
+
