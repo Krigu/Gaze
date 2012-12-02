81749583fd756c9b4e39881a36d9d98ced8ef4ce
Krigu <chris.feuz@gmail.com>
2012-12-02 21:13:13 +0100

    Added ImageSource for video camera

---
+++ GazeLib/video/LiveSource.hpp
@@ -0,0 +1,26 @@
+/*
+ * LiveSource.h
+ *
+ *  Created on: Dec 2, 2012
+ *      Author: krigu
+ */
+
+#ifndef LIVESOURCE_H_
+#define LIVESOURCE_H_
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include "ImageSource.hpp"
+
+class LiveSource : public ImageSource {
+private:
+	cv::VideoCapture videoCapture;
+public:
+	LiveSource();
+	LiveSource(int channel);
+	virtual ~LiveSource();
+	virtual bool nextGrayFrame(cv::Mat& frame);
+};
+
+#endif /* LIVESOURCE_H_ */
--- GazeTracker/StartGazeTracker.cpp
+++ GazeTracker/StartGazeTracker.cpp
@@ -10,23 +10,56 @@
 #include "detection/GazeTracker.hpp"
 #include "detection/pupil/Starburst.hpp"
 #include "video/VideoSource.hpp"
+#include "video/LiveSource.hpp"
 #include "GazeConstants.hpp"
 
 
 using namespace std;
+using namespace cv;
 
 int main(){
 
-	string path = GazeConstants::inHomeDirectory("Dropbox/gaze/videos/osx/krigu_cut.mov");
+//	string path = GazeConstants::inHomeDirectory("Dropbox/gaze/videos/osx/krigu_cut.mov");
+//
+//
+//	cout << path << endl;
+//
+//	LiveSource source;
+//
+//	GazeTracker tracker(source);
+//
+//	tracker.startTracking();
+
+	VideoCapture cap(0);
+
+	if(!cap.isOpened())
+		return 0;
 
 
-	cout << path << endl;
+	int numOfMeasures = 0;
 
-	VideoSource videoSource(path);
+	Mat frame;
+	namedWindow("test", 1);
+	double totalTime = 0;
 
-	GazeTracker tracker(videoSource);
+	while(true){
+		double t = getTickCount();
 
-	tracker.startTracking();
+		cap >> frame;
+		imshow("test", frame);
+
+		if(waitKey(30) == 27) // ESCAPE
+			break;
+
+		totalTime += ((double) getTickCount() - t) / getTickFrequency();
+		numOfMeasures++;
+		if (numOfMeasures >= 1000) {
+			cout << "Average processing time in seconds(" << numOfMeasures
+					<< " measures): "  << totalTime / numOfMeasures << endl;
+			numOfMeasures = 0;
+			totalTime = 0;
+		}
+	}
 
 
 }
