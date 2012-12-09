af3ad7eeaab8988ccf58975bea3d797fd1e3b626
Krigu <chris.feuz@gmail.com>
2012-12-09 12:51:47 +0100

    Added exception messages

--- GazeLib/exception/GazeExceptions.cpp
+++ GazeLib/exception/GazeExceptions.cpp
@@ -7,21 +7,30 @@
 
 #include "GazeExceptions.hpp"
 
-GazeException::GazeException() {
+GazeException::GazeException(std::string msg) : errorMsg(msg) {
 }
 
-GlintNotFoundException::GlintNotFoundException() {
+GazeException::~GazeException() throw () {
+
+}
+
+const char * GazeException::what() const throw () {
+    return errorMsg.c_str();
+}
+
+GlintNotFoundException::GlintNotFoundException() : GazeException("Glint not found\nPlease check if the eye is still in the camera focus!") {
 
 };
 
-PupilNotFoundException::PupilNotFoundException() {
+PupilNotFoundException::PupilNotFoundException() : GazeException("Pupil not found\nPlease check if the eye is still in the camera focus!") {
 
 };
 
-EyeRegionNotFoundException::EyeRegionNotFoundException() {
+EyeRegionNotFoundException::EyeRegionNotFoundException() : GazeException("EyeRegion not found\nPlease check if the eye is still in the camera focus!") {
 
 };
 
-NoImageSourceException::NoImageSourceException(){
-      
+NoImageSourceException::NoImageSourceException() : GazeException("No image source \nPlease check whether the camera is connected to the system!") {
+
 };
+
--- GazeLib/exception/GazeExceptions.hpp
+++ GazeLib/exception/GazeExceptions.hpp
@@ -9,37 +9,41 @@
 #define	GAZEEXCEPTIONS_HPP
 
 #include <exception>
+#include <string>
 
 class GazeException : public std::exception {
-public:
-    GazeException();
 private:
+    std::string errorMsg;
+
+public:
+    GazeException(std::string msg);
+    ~GazeException() throw ();
+    const char *what() const throw ();
 
 };
 
-class GlintNotFoundException : public GazeException{
+class GlintNotFoundException : public GazeException {
 public:
     GlintNotFoundException();
 
 };
 
-class PupilNotFoundException : public GazeException{
+class PupilNotFoundException : public GazeException {
 public:
     PupilNotFoundException();
 };
 
-class EyeRegionNotFoundException : public GazeException{
+class EyeRegionNotFoundException : public GazeException {
 public:
     EyeRegionNotFoundException();
 };
 
-class FaceRegionNotFoundException : public GazeException{
+class FaceRegionNotFoundException : public GazeException {
 public:
     FaceRegionNotFoundException();
 };
 
-
-class NoImageSourceException : public GazeException{
+class NoImageSourceException : public GazeException {
 public:
     NoImageSourceException();
 };
