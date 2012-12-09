528ca1d20b5af847fdd803ea10717fb84ecbf57a
Krigu <chris.feuz@gmail.com>
2012-12-09 21:22:55 +0100

    Added two phase calibration

--- GazeTracker/StartGazeTracker.cpp
+++ GazeTracker/StartGazeTracker.cpp
@@ -10,23 +10,27 @@
 #include "detection/GazeTracker.hpp"
 #include "detection/pupil/Starburst.hpp"
 #include "video/VideoSource.hpp"
-#include "GazeConstants.hpp"
+#include "video/LiveSource.hpp"
+#include "config/GazeConstants.hpp"
 
 
 using namespace std;
 
 int main(){
 
-	string path = GazeConstants::inHomeDirectory("Dropbox/gaze/videos/osx/krigu_cut.mov");
+	string path = GazeConstants::inHomeDirectory("Dropbox/gaze/videos/k2.webm");
 
 
 	cout << path << endl;
 
 	VideoSource videoSource(path);
+    
+    LiveSource liveSource;
 
 	GazeTracker tracker(videoSource);
 
-	tracker.startTracking();
+	tracker.initializeCalibration();    
+    tracker.track();
 
 
 }
