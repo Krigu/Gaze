8119891de391525428de9358358b2e4281b37f89
Flurin Rindisbacher <info@flurischt.ch>
2012-12-09 19:16:35 +0100

    no longer use constants for our configuration
    these variables are now all (configurable) statics

--- GazeLib/config/GazeConstants.cpp
+++ GazeLib/config/GazeConstants.cpp
@@ -11,14 +11,42 @@
 
 using namespace std;
 
-GazeConstants::GazeConstants() {
-	// TODO Auto-generated constructor stub
+//
+// global constants
+//
+const char GazeConstants::GLINT_COUNT;
+const unsigned short GazeConstants::MAX_RANSAC_ITERATIONS;
+const unsigned short GazeConstants::NUM_OF_SMOOTHING_FRAMES;
 
-}
+//
+// Glints Configuration
+//
 
-GazeConstants::~GazeConstants() {
-	// TODO Auto-generated destructor stub
-}
+double GazeConstants::GLINT_THRESHOLD;
+short GazeConstants::GLINT_MIN_DIAMETER;
+short GazeConstants::GLINT_MAX_DIAMETER;
+short GazeConstants::GLINT_MIN_PIXEL;
+short GazeConstants::GLINT_MAX_PIXEL;
+short GazeConstants::GLINT_MIN_DISTANCE;
+short GazeConstants::GLINT_MAX_DISTANCE;
+
+//
+// Haar configuration
+//
+
+short GazeConstants::HAAR_EYEREGION_MAX_HEIGHT;
+short GazeConstants::HAAR_EYEREGION_MAX_WIDTH;
+short GazeConstants::HAAR_EYE_MIN_HEIGHT;
+short GazeConstants::HAAR_EYE_MIN_WIDTH;
+short GazeConstants::HAAR_EYE_MAX_HEIGHT;
+short GazeConstants::HAAR_EYE_MAX_WIDTH;
+short GazeConstants::HAAR_FINDREGION_MAX_TRIES;
+
+//
+// RANSAC stuff
+//
+short GazeConstants::PUPIL_MIN_RADIUS;
+short GazeConstants::PUPIL_MAX_RADIUS;
 
 string GazeConstants::inHomeDirectory(string suffix) {
 	string home(getenv("HOME"));
--- GazeLib/config/GazeConstants.hpp
+++ GazeLib/config/GazeConstants.hpp
@@ -12,68 +12,117 @@
 
 class GazeConstants {
 public:
-	static const double GLINT_THRESHOLD = 230;
+    
+    //
+    // global constants
+    //
+    
+	// Amounts of glints (per eye)
+	static const char GLINT_COUNT = 4;
+
+	static const unsigned short MAX_RANSAC_ITERATIONS = 10;
+
+	// how many frames should we use to smooth the signal?
+	static const unsigned short NUM_OF_SMOOTHING_FRAMES = 5;
+    
+	static const short GLINT_RADIUS = 6;
 
 	//
 	// Glints Configuration
 	//
-
-	// Amounts of glints (per eye)
-	static const char GLINT_COUNT = 4;
-
-	static const short GLINT_MIN_DIAMETER = 2;
-	static const short GLINT_MAX_DIAMETER = 6;
-
-	static const short GLINT_RADIUS = 6;
+    
+    // fasch ds wichtigschte
+	static double GLINT_THRESHOLD;
+    
+	static short GLINT_MIN_DIAMETER;
+	static short GLINT_MAX_DIAMETER;
 	
     // TODO calculate from diameter
-	static const short GLINT_MIN_PIXEL = 1;
+	static short GLINT_MIN_PIXEL;
 	// TODO calculate from diameter
-	static const short GLINT_MAX_PIXEL = 50;
+	static short GLINT_MAX_PIXEL;
 	// Min distance between two glints
-	static const short GLINT_MIN_DISTANCE = 1;
+	static short GLINT_MIN_DISTANCE;
 	// Max distance between two glints
-	static const short GLINT_MAX_DISTANCE = 40;
+	static short GLINT_MAX_DISTANCE;
 
 	//
 	// Haar configuration
 	//
 
 	// Max height of eyeregion
-	static const short HAAR_EYEREGION_MAX_HEIGHT = 100;
+	static short HAAR_EYEREGION_MAX_HEIGHT;
 	// Max width of eyeregion
-	static const short HAAR_EYEREGION_MAX_WIDTH = 400;
+	static short HAAR_EYEREGION_MAX_WIDTH;
 	// Max height of eye
-	static const short HAAR_EYE_MIN_HEIGHT = 20;
+	static short HAAR_EYE_MIN_HEIGHT;
 	// Max width of eye
-	static const short HAAR_EYE_MIN_WIDTH = 20;
+	static short HAAR_EYE_MIN_WIDTH;
 	// Max height of eye
-	static const short HAAR_EYE_MAX_HEIGHT = 200;
+	static short HAAR_EYE_MAX_HEIGHT;
 	// Max width of eye
-	static const short HAAR_EYE_MAX_WIDTH = 200;
+	static short HAAR_EYE_MAX_WIDTH;
 	// Max amount of tries to find eye region
-	static const short HAAR_FINDREGION_MAX_TRIES = 10;
+	static short HAAR_FINDREGION_MAX_TRIES;
 
+    //
 	// RANSAC stuff
-	static const short PUPIL_MIN_RADIUS = 15;
-	static const short PUPIL_MAX_RADIUS = 17;
+	//
+    static short PUPIL_MIN_RADIUS;
+	static short PUPIL_MAX_RADIUS;
 
-	static const unsigned short MAX_RANSAC_ITERATIONS = 10;
+	GazeConstants(){
+        initConiguration();
+    }
 
-	// how many frames should we use to smooth the signal?
-	static const unsigned short NUM_OF_SMOOTHING_FRAMES = 5;
+    ~GazeConstants(){
+        
+    }
+    
+    void initConiguration(){
+        
+        //
+        // Glints Configuration
+        //
 
+        GazeConstants::GLINT_THRESHOLD = 230;
+        GazeConstants::GLINT_MIN_DIAMETER = 2;
+        GazeConstants::GLINT_MAX_DIAMETER = 6;
 
-	GazeConstants();
+        //static const short GLINT_RADIUS = 6;
 
+        GazeConstants::GLINT_MIN_PIXEL = 1;
+        GazeConstants::GLINT_MAX_PIXEL = 50;
+        GazeConstants::GLINT_MIN_DISTANCE = 1;
+        GazeConstants::GLINT_MAX_DISTANCE = 40;
+        
+        //
+        // Haar configuration
+        //
+
+        GazeConstants::HAAR_EYEREGION_MAX_HEIGHT = 100;
+        GazeConstants::HAAR_EYEREGION_MAX_WIDTH = 400;
+        GazeConstants::HAAR_EYE_MIN_HEIGHT = 20;
+        GazeConstants::HAAR_EYE_MIN_WIDTH = 20;
+        GazeConstants::HAAR_EYE_MAX_HEIGHT = 200;
+        GazeConstants::HAAR_EYE_MAX_WIDTH = 200;
+        GazeConstants::HAAR_FINDREGION_MAX_TRIES = 10;
+        
+        //
+        // RANSAC stuff
+        //
+        GazeConstants::PUPIL_MIN_RADIUS = 15;
+        GazeConstants::PUPIL_MAX_RADIUS = 17;
+    }
+    
 	/**
 	 * returns a path to the users homedirectory
 	 *
 	 * you may add a suffix for example to point directly to a file
 	 */
 	static std::string inHomeDirectory(std::string suffix);
-
-	virtual ~GazeConstants();
 };
+
+static GazeConstants GAZE_CONSTANTS;
 
 #endif /* GAZECONSTANTS_HPP_ */
