dbd3da2e9467fed37798e41f491886f1df087622
Krigu <chris.feuz@gmail.com>
2012-12-09 21:32:37 +0100

    Little merge correction I forgot in previous commit

--- GazeTracker/StartGazeTracker.cpp
+++ GazeTracker/StartGazeTracker.cpp
@@ -10,12 +10,9 @@
 #include "detection/GazeTracker.hpp"
 #include "detection/pupil/Starburst.hpp"
 #include "video/VideoSource.hpp"
-<<<<<<< HEAD
 #include "video/LiveSource.hpp"
-#include "config/GazeConstants.hpp"
-=======
 #include "config/GazeConfig.hpp"
->>>>>>> 65a94e832133cc233f02475eec2b7403880aa4d2
+
 
 
 using namespace std;
