9c151f96834ffbe8cd6fe338199073c2832b4312
Krigu <chris.feuz@gmail.com>
2012-12-09 12:52:59 +0100

    Changed move-method.
    If target and current position is identical, then no animation will be shown.

--- GazeBrowser/html/js/calibration.js
+++ GazeBrowser/html/js/calibration.js
@@ -1,48 +1,58 @@
 var calibrationCircle = {
 	
-	imgPath: "img/circle_full.jpg", //url to image here
-	imgMovingPath: "img/mario.gif", //url to moving image here
-	imgWidth: 40, //standard width
-	imgHeight: 40, //standard height
-	animationTime: 2500, // time for the animation
+    imgPath: "img/circle_full.jpg", //url to image here
+    imgMovingPath: "img/mario.gif", //url to moving image here
+    imgWidth: 40, //standard width
+    imgHeight: 40, //standard height
+    animationTime: 2500, // time for the animation
 		
-	init : function(posX, posY){
-		//create the image cloud
-		var circle = document.createElement("IMG");
-		$(circle).attr("src", calibrationCircle.imgPath);
-		$(circle).attr("alt", "circle");
-		$(circle).attr("class", "circle");
-		$(circle).attr("id", "circleImg");
+    init : function(posX, posY){
+        //create the image cloud
+        var circle = document.createElement("IMG");
+        $(circle).attr("src", calibrationCircle.imgPath);
+        $(circle).attr("alt", "circle");
+        $(circle).attr("class", "circle");
+        $(circle).attr("id", "circleImg");
+        $(circle).css("top", "20px");
+        $(circle).css("left", "20px");
 
-		$(document.body).append($(circle));
+        $(document.body).append($(circle));
 
-		calibrationCircle.display(posX, posY);		
+        calibrationCircle.display(posX, posY);		
 
-	},
+    },
 	
-	display : function(posX, posY) {
-		$("#circleImg").css("top", posY - (calibrationCircle.imgHeight / 2));
-		$("#circleImg").css("left", posX - (calibrationCircle.imgWidth / 2));
-		$("#circleImg").show(0);
-	},
+    display : function(posX, posY) {
+        $("#circleImg").css("top", posY - (calibrationCircle.imgHeight / 2));
+        $("#circleImg").css("left", posX - (calibrationCircle.imgWidth / 2));
+        $("#circleImg").show(0);
+    },
 
-	move : function(posX, posY){
-		$("#circleImg").attr("src", calibrationCircle.imgMovingPath);
-		$("#circleImg").animate({ 
-			top: posY - (calibrationCircle.imgHeight / 2),
-			left: posX - (calibrationCircle.imgWidth / 2)
-		}, calibrationCircle.animationTime, function() {
-			$("#circleImg").attr("src", calibrationCircle.imgPath);
-		 });
+    move : function(posX, posY){
+        var x = posX - (calibrationCircle.imgWidth / 2);
+        var y = posY - (calibrationCircle.imgHeight / 2);
+        
+        // Dont show animation if position stays the same
+        if (x + "px" == $("#circleImg").css("left") || 
+            y + "px" == $("#circleImg").css("top"))
+            return;
+        
+        $("#circleImg").attr("src", calibrationCircle.imgMovingPath);
+        $("#circleImg").animate({ 
+            top: y,
+            left: x
+        }, calibrationCircle.animationTime, function() {
+            $("#circleImg").attr("src", calibrationCircle.imgPath);
+        });
 	
-	},
+    },
 
-	hide : function(){
-		$("#circleImg").hide();
-	}
+    hide : function(){
+        $("#circleImg").hide();
+    }
 }
 
 $(document).ready(function() {
-	   calibrationCircle.init();
+    calibrationCircle.init();
 });
 
