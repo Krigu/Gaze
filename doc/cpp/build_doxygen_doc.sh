fcafa39e3b6371f65bcb6a5d3ff042c2a24391d7
Flurin Rindisbacher <info@flurischt.ch>
2012-12-30 17:44:02 +0100

    add the documentation directory and a helper script for doxygen

---
+++ doc/cpp/build_doxygen_doc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# this script builds the doxygen documentation
+# for every project that is configured in the 
+# doxygen_conf/ directory. 
+#
+# make sure, that all paths in the doxygen config-files
+# are relative to the directory of this script!
+#
+# how to bild a doxy-config file:
+# cd __doxygen_conf && doxygen -g PROJECT_FILENAME
+#
+
+WORKING_DIR=$(dirname $0)
+
+# run doxygen for every configured project
+for project in `ls $WORKING_DIR/__doxygen_conf`; do
+    mkdir -p $WORKING_DIR/$project
+    cd $WORKING_DIR && doxygen __doxygen_conf/$project
+done
+
