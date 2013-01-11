11f4b83b960c22a9a8dddd08df13d630c1cb1ed5
Krigu <chris.feuz@gmail.com>
2013-01-11 11:02:17 +0100

    smaller corrections and adaptions to the state machine.

--- GazeBrowser/threads/ThreadManager.hpp
+++ GazeBrowser/threads/ThreadManager.hpp
@@ -18,9 +18,8 @@ class BrowserWindow;
 class IdleWorker;
 
 class ThreadManager : public QObject {
-    
     Q_OBJECT
-    
+
 public:
     ThreadManager(BrowserWindow *parent);
     virtual ~ThreadManager();
@@ -28,35 +27,35 @@ public:
     void goIdle();
     void resumeTracking();
     bool isCalibrated();
-    
+
 public slots:
     void error(QString message);
     void info(QString message);
     void calibrationFinished();
     void threadStopped(PROGRAM_STATES nextState);
-    
+
 private:
-    
+
     BrowserWindow *parent;
     PROGRAM_STATES state;
-    
+
     // our threads "application logic"
     GazeTrackWorker *gazeTracker;
     IdleWorker *idle;
-    
+
     // the threads
     QThread *gazeTrackerThread;
     QThread *idleThread;
-    
+
     // the Camera-Lock
     QMutex *cameraLock;
-    
+
     // the state transitions for the state machine
     state_transitions *transitions;
     int num_of_transitions;
-    
+
     void setUpSignalHandling();
-    
+
     void fsmSetupStateMachine();
     bool fsmProcessEvent(PROGRAM_EVENTS event);
     void fsmGoIdle(PROGRAM_STATES nextState);
@@ -65,7 +64,7 @@ private:
     void fsmStopIdle(PROGRAM_STATES nextState);
     void fsmStopGazeTracking(PROGRAM_STATES nextState);
     void fsmPermanentError(PROGRAM_STATES nextState);
-    
+
     // copying the ThreadManager makes absolutely no sense, 
     // let's therefore disable it
     ThreadManager(const ThreadManager& orig);
--- GazeBrowser/threads/worker/GazeTrackWorker.cpp
+++ GazeBrowser/threads/worker/GazeTrackWorker.cpp
@@ -12,149 +12,150 @@
 
 using namespace std;
 
-GazeTrackWorker::GazeTrackWorker(int width, int height, ImageSource *camera, QMutex *cameraLock) 
-       : width(width), height(height), camera(camera), cameraLock(cameraLock), running(false){
-    
+GazeTrackWorker::GazeTrackWorker(int width, int height, ImageSource *camera, QMutex *cameraLock)
+: width(width), height(height), camera(camera), cameraLock(cameraLock), running(false) {
+
     tracker = new GazeTracker(*camera, this);
     calibration = NULL;
-    
+
 }
 
-GazeTrackWorker::~GazeTrackWorker(){
+GazeTrackWorker::~GazeTrackWorker() {
     delete tracker;
-    if(calibration != NULL)
+    if (calibration != NULL)
         delete calibration;
 }
 
-void GazeTrackWorker::startCalibration()
-{   
-    if(!cameraLock->tryLock()){
+void GazeTrackWorker::startCalibration() {
+    if (!cameraLock->tryLock()) {
         emit error(tr("Cannot calibrate, is the camera in use?"));
         return;
     }
-    
+
     running = true;
     tracking = false;
-    
+
     // is this a recalibration?
-    if(calibration !=NULL){
+    if (calibration != NULL) {
         delete calibration;
-        calibration=NULL;
+        calibration = NULL;
     }
-    
-    try{
-        bool calibrated=false;
-        while(!calibrated && running){
+
+    try {
+        bool calibrated = false;
+        while (!calibrated && running) {
             calibration = new Calibration;
             calibrated = calibrate();
-            if(!calibrated)
+            if (!calibrated)
                 delete calibration;
         }
-   
+
         cameraLock->unlock();
-        
-        if(calibrated)
+
+        if (calibrated)
             emit(calibrationFinished());
         else if (!running)
             emit hasStopped(nextStateAfterStop);
-        
-    } catch(GazeException& e) {
+
+    } catch (GazeException& e) {
         cameraLock->unlock();
         emit error(e.what());
     }
 }
 
-bool GazeTrackWorker::isCalibrated(){
+bool GazeTrackWorker::isCalibrated() {
     return calibration != NULL;
 }
 
-void GazeTrackWorker::startTracking(){
-    
-    if(!isCalibrated()){
+void GazeTrackWorker::startTracking() {
+
+    if (!isCalibrated()) {
         emit error(tr("Please Calibrate the System before Tracking!"));
         return;
     }
-    
-    if(!cameraLock->tryLock()){
+
+    if (!cameraLock->tryLock()) {
         emit error(tr("Cannot track, is the camera in use?"));
         return;
     }
-    
+
+    // Wait with begining of tracking until bookmark page is loaded
+    Sleeper::msleep(2000);
+
     running = true;
     tracking = true;
-    
-    while(running){
-        try{
+
+    while (running) {
+        try {
 
             tracker->track();
 
-        } catch(FaceRegionNotFoundException &e){
+        } catch (FaceRegionNotFoundException &e) {
             emit info(tr("No Face detected - Please hold your head still!"));
             tracker->initializeCalibration();
-        } catch(EyeRegionNotFoundException &e){
+        } catch (EyeRegionNotFoundException &e) {
             emit info(tr("No Eye detected - Please hold your head still!"));
             tracker->initializeCalibration();
-        }catch(GazeException &e){
+        } catch (GazeException &e) {
             emit error(e.what());
             break; // leave 
         }
     }
-    
+
     cameraLock->unlock();
-    
+
 }
 
-bool GazeTrackWorker::calibrate(){
-     int x_offset = 40;
-     int y_offset = 40;
-     
-     int height = this->height - 2 * y_offset;
-     int width =  this->width - 2 * x_offset;
-     
-     tracker->initializeCalibration();
+bool GazeTrackWorker::calibrate() {
+    int x_offset = 40;
+    int y_offset = 40;
 
-     for(unsigned short i=0;i<3 && running;i++){
-         for(unsigned short j=0;j<3 && running;j++){
-            int point_x = width / 2 * j + x_offset; 
+    int height = this->height - 2 * y_offset;
+    int width = this->width - 2 * x_offset;
+
+    tracker->initializeCalibration();
+
+    for (unsigned short i = 0; i < 3 && running; i++) {
+        for (unsigned short j = 0; j < 3 && running; j++) {
+            int point_x = width / 2 * j + x_offset;
             int point_y = height / 2 * i + y_offset;
-             
+
             cout << "Calibration Point: " << point_x << "/" << point_y << endl;
-            
+
             QString code = QString("calibrationCircle.move(%1,%2);")
                     .arg(point_x).arg(point_y);
-            
+
             emit jsCommand(code);
             Sleeper::msleep(3000);
-            
+
             Point2f p(point_x, point_y);
             measurements.clear();
             tracker->track(5);
             CalibrationData data(p, measurements);
             calibration->addCalibrationData(data);
             cout << "Point: " << p << "Vector: " << data.getMeasuredMedianVector() << endl;
-         }
-     }
-     
-     if(running)
-        return calibration->calibrate(100,3);
-     else
-         return false;
+        }
+    }
+
+    if (running)
+        return calibration->calibrate(100, 3);
+    else
+        return false;
 }
 
-bool GazeTrackWorker::imageProcessed(Mat& resultImage){
+bool GazeTrackWorker::imageProcessed(Mat& resultImage) {
     emit cvImage(new Mat(resultImage));
     return running;
 }
 
-bool GazeTrackWorker::imageProcessed(Mat& resultImage, MeasureResult &result, Point2f &gazeVector){
-    if(result == MEASURE_OK){
-        if(tracking){
+bool GazeTrackWorker::imageProcessed(Mat& resultImage, MeasureResult &result, Point2f &gazeVector) {
+    if (result == MEASURE_OK) {
+        if (tracking) {
             emit estimatedPoint(calibration->calcCoordinates(gazeVector));
         } else {
             measurements.push_back(gazeVector);
         }
     }
-    
     if (!tracking)
         emit cvImage(new Mat(resultImage));
 
@@ -163,7 +164,7 @@ bool GazeTrackWorker::imageProcessed(Mat& resultImage, MeasureResult &result, Po
     return running;
 }
 
-void GazeTrackWorker::stop(PROGRAM_STATES nextState){
+void GazeTrackWorker::stop(PROGRAM_STATES nextState) {
     this->nextStateAfterStop = nextState;
     this->running = false;
 }
