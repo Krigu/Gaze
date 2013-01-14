d46453330123e066bf84c86058bb7041e4e0bde9
Flurin Rindisbacher <info@flurischt.ch>
2013-01-14 19:48:03 +0100

    cleanup and document the exceptions

--- GazeLib/exception/GazeExceptions.cpp
+++ GazeLib/exception/GazeExceptions.cpp
@@ -1,10 +1,3 @@
-/* 
- * File:   GazeExceptions.cpp
- * Author: krigu
- * 
- * Created on December 7, 2012, 2:09 PM
- */
-
 #include "GazeExceptions.hpp"
 #include "../utils/log.hpp"
 
@@ -19,14 +12,6 @@ GazeException::~GazeException() throw () {
 const char * GazeException::what() const throw () {
     return errorMsg.c_str();
 }
-
-GlintNotFoundException::GlintNotFoundException() : GazeException("Glint not found\nPlease check if the eye is still in the camera focus!") {
-
-};
-
-PupilNotFoundException::PupilNotFoundException() : GazeException("Pupil not found\nPlease check if the eye is still in the camera focus!") {
-
-};
 
 EyeRegionNotFoundException::EyeRegionNotFoundException() : GazeException("EyeRegion not found\nPlease check if the eye is still in the camera focus!") {
 
--- GazeLib/exception/GazeExceptions.hpp
+++ GazeLib/exception/GazeExceptions.hpp
@@ -1,16 +1,14 @@
-/* 
- * File:   GazeExceptions.hpp
- * Author: krigu
- *
- * Created on December 7, 2012, 2:09 PM
- */
-
 #ifndef GAZEEXCEPTIONS_HPP
 #define	GAZEEXCEPTIONS_HPP
 
 #include <exception>
 #include <string>
 
+/**
+ *
+ * GazeException is the parent class of all Exceptions thrown in GazeLib.
+ * when a GazeExceptino is thrown, the reason is ALWAYS printed to stdout
+ */
 class GazeException : public std::exception {
 private:
     std::string errorMsg;
@@ -22,32 +20,28 @@ public:
 
 };
 
-class GlintNotFoundException : public GazeException {
-public:
-    GlintNotFoundException();
-
-};
-
-class PupilNotFoundException : public GazeException {
-public:
-    PupilNotFoundException();
-};
-
+/**
+ * EyeRegionNotFoundException is thrown when within HAAR_FINDREGION_MAX_TRIES
+ * tries no eye region has been found. @see HAAR_FINDREGION_MAX_TRIES
+ */
 class EyeRegionNotFoundException : public GazeException {
 public:
     EyeRegionNotFoundException();
 };
 
-class FaceRegionNotFoundException : public GazeException {
-public:
-    FaceRegionNotFoundException();
-};
-
+/**
+ * NoImageSourceException is thrown when a LiveSource or VideoSource is opened
+ * with the wrong parameters. this could be an non existent video file
+ * or a wrong channel for an usb camera.
+ */
 class NoImageSourceException : public GazeException {
 public:
     NoImageSourceException();
 };
 
+/**
+ * WrongArgumentExcepion means that a client of GazeLib misused one of the API calls.
+ */
 class WrongArgumentException : public GazeException {
 public:
     WrongArgumentException(std::string msg);
