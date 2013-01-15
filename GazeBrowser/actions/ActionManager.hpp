a260cd5efa77cbbef3799a6bf3f054a13f60c7fe
Flurin Rindisbacher <info@flurischt.ch>
2013-01-15 22:30:23 +0100

    documents the actions package

--- GazeBrowser/actions/ActionManager.hpp
+++ GazeBrowser/actions/ActionManager.hpp
@@ -9,21 +9,50 @@
 
 #include "GazeAction.hpp"
 
+/**
+ * The ActionManager class is responsible for enabling / disabling the GazeActions.
+ * the slot estimatedPoint(cv::Point) is called for each measured Screen position. 
+ * the manager then calculates which Action is triggered by this measurement and focuses it.
+ * using a mode the ActionManager can trigger different GazeActions for the same screen position
+ */
 class ActionManager : public QObject {
     Q_OBJECT
 
 public:
+    /**
+     * 
+     * @param actionMap a map of all GazeActions and their position (rect) on the screen
+     */
     ActionManager(std::map<int, std::vector<GazeAction*> > actionMap);
     virtual ~ActionManager();
 
+    /**
+     * removes all actions
+     */
     void clearActions();
+    
+    /**
+     * sets the current mode of the ActionManager
+     * @param mode
+     */
     void setMode(int mode);
     
-    
+    /**
+     * when paused all incoming GazeVectors (estimated points) are silently discarded
+     */
     void pause();
+    
+    /**
+     * resumes the processing of the incoming measurements
+     */
     void resume();   
 
 public slots:
+    
+    /**
+     * accepts measured Gaze positions and executes an action (if any)
+     * @param p the position on the screen where the user is looking at
+     */
     void estimatedPoint(cv::Point p);
 
 private:
--- GazeBrowser/actions/GazeAction.hpp
+++ GazeBrowser/actions/GazeAction.hpp
@@ -6,24 +6,46 @@
 #include <opencv2/core/core.hpp>
 #include <QtCore>
 
-
-
 class GazePointer;
 class BrowserWindow;
 
 typedef void (BrowserWindow::*commitAction)(cv::Point);
 
+/**
+ *  a GazeAction is an action that can be executed with a Gaze Tracker. each 
+ * GazeAction has a position on the screen (usually a rect) where it can be enabled. 
+ * if a user focuses the GazeAction for a defined time the action is triggered and 
+ * a callback function is executed.
+ */
 class GazeAction : public QObject {
     Q_OBJECT
 
 public:
+    /**
+     * 
+     * @param name the name of this GazeAction
+     * @param region the region where the action can be triggered
+     * @param prepareTimeInMillis the time after which the GazePointer is displayed
+     * @param commitTimeInMillis the time after which the action is executed
+     * @param browserWindow a reference to the browser
+     * @param callback the callback function that is executed after commitTime
+     * @param gazePointer a reference to the GazePointer
+     */
     GazeAction(std::string name, cv::Rect region, int prepareTimeInMillis, int commitTimeInMillis, BrowserWindow *browserWindow, commitAction callback, GazePointer *gazePointer);
     virtual ~GazeAction();
 
     std::string getActionName() const;
     cv::Rect getRegion() const;
 
+    /**
+     * starts measuring the prepare and commit time<br />commits the action
+     * if the commit time has been exceeded
+     */
     void focus();
+    
+    /**
+     * stops measuring the prepare and commit time
+     */
     void unfocus();
 
 private:
